797fdbcfe890e207b419ff6951ee60b6a3a2486b
Lihao Lei <leilei199708@gmail.com>
2020-12-17 22:13:19 +0800

    update

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ add_action('after_setup_theme', 'intimacy_setup' );
 // register a widget area
 function intimacy_widgets_init() {
 	global $wp_widget_factory;
-	remove_action('wp_head', array($wp_widget_factory ->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
+	remove_action('wp_head', array($wp_widget_factory -> widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
 
 	register_sidebar(
 		array(
@@ -42,8 +42,8 @@ function intimacy_scripts() {
 }
 add_action('wp_enqueue_scripts', 'intimacy_scripts');
 
-remove_action('wp_head','rsd_link');//移除head中的rel="EditURI"
-remove_action('wp_head','wlwmanifest_link');//移除head中的rel="wlwmanifest"
+remove_action('wp_head','rsd_link');
+remove_action('wp_head','wlwmanifest_link');
 remove_action('wp_head', 'wp_generator'); // remove wp version info
 
 // remove wp-json link
@@ -52,7 +52,7 @@ add_filter('rest_jsonp_enabled', '_returned_false');
 remove_action('wp_head', 'rest_output_link_wp_head', 10);
 remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
 
-//删除emoji脚本
+// remove wp-emoji
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script');
 remove_action( 'admin_print_styles', 'print_emoji_styles');
 remove_action( 'wp_head', 'print_emoji_detection_script', 7);
