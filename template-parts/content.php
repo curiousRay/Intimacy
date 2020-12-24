757abb8eef07bc85f06b03bf3d9d50e7a0e06eb4
Lihao Lei <leilei199708@gmail.com>
2020-12-24 20:47:37 +0800

    remove update time

--- template-parts/content.php
+++ template-parts/content.php
@@ -34,20 +34,18 @@
 		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
 		}
 
 		$time_string = sprintf(
 			$time_string,
 			esc_attr( get_the_date( 'c' ) ),
-			get_the_date(),
-			esc_attr( get_the_modified_date( 'c' ) ),
-			get_the_modified_date()
+			get_the_date()
 		);
 
 		printf(
 			'<span class="posted-on"><span class="screen-reader-text">%1$s </span><a href="%2$s" rel="bookmark">%3$s</a></span>',
-			_x( 'Posted on', 'Used before publish date.', 'twentysixteen' ),
+			_x( 'Posted on', 'Used before publish date.', 'intimacy' ),
 			esc_url( get_permalink() ),
 			$time_string
 		);
