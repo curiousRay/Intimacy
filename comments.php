2d247c1a54a0d21562a5492d73fc48fead729b12
Lihao Lei <leilei199708@gmail.com>
2020-12-24 23:59:41 +0800

    able to customize comment form submit btn

--- comments.php
+++ comments.php
@@ -103,7 +103,17 @@ if ( post_password_required() ) {
     .'<label for="wp-comment-cookies-consent">'
     .__('Remenber me for the next time I comment', 'textdomain')
     .'</label></p>';
-    
+
+//     // define the comment_form_submit_button callback
+// function filter_comment_form_submit_button( $submit_button, $args ) {
+//   // make filter magic happen here...
+//   $submit_before = '<div class="form-group">';
+//   $submit_after = '</div>';
+//   return $submit_before . $submit_button . $submit_after;
+// };
+// // add the filter
+// add_filter( 'comment_form_submit_button', 'filter_comment_form_submit_button', 10, 2 );
+
     $comments_args = array(
       'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
       'title_reply_after'  => '</h2>',
