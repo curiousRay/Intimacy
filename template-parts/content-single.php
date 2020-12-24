4c15d01a120c52f2a38b1647261158ebe797eb9f
Lihao Lei <leilei199708@gmail.com>
2020-12-24 21:46:47 +0800

    implemented comments

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -4,7 +4,7 @@
  */
 
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class('content-single-box'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('content-single-card'); ?>>
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 	</header><!-- .entry-header -->
