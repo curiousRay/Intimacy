6c2cd9f8b66cc685024ec0b026d01add8d26820d
Lihao Lei <leilei199708@gmail.com>
2021-01-23 00:36:18 +0800

    modify layout

--- 404.php
+++ 404.php
@@ -10,13 +10,12 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentysixteen' ); ?></h1>
+					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'intimacy' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentysixteen' ); ?></p>
+					<p><?php _e( 'It looks like nothing was found at this location.', 'intimacy' ); ?></p>
 
-					<?php get_search_form(); ?>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
