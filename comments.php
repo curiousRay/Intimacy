4fe368122bea05f0ebcb21f23794b592c6f93cc1
Lihao Lei <leilei199708@gmail.com>
2021-03-13 19:41:26 +0800

    update comment style

--- comments.php
+++ comments.php
@@ -61,24 +61,25 @@ if ( post_password_required() ) {
     $GLOBALS['comment'] = $comment; ?>
     <li <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
       <div id="comment-<?php comment_ID(); ?>">
-       <div class="comment-author vcard">
+       <div class="vcard">
           <?php echo get_avatar($comment,$size='36',$default='<path_to_url>' ); ?>
-  
-          <?php printf(__('<cite class="fn">%s</cite> <span class="says">says:</span>'), get_comment_author_link()) ?>
+
+          <div class="comment-author">
+          <?php printf(__('<cite class="fn">%s</cite>'), get_comment_author_link()) ?>
+            <div class="comment-datetime"">
+               <?php printf(__('%1$s at %2$s'), get_comment_date(),  get_comment_time()) ?>
+            
+            <?php edit_comment_link(__('(Edit)'),'  ','') ?>
+          </div>
+        </div>
+
        </div>
        <?php if ($comment->comment_approved == '0') : ?>
           <em><?php _e('Your comment is awaiting moderation.') ?></em>
           <br />
        <?php endif; ?>
   
-       <div class="comment-meta commentmetadata">
-           <a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>">
-               <?php printf(__('%1$s at %2$s'), get_comment_date(),  get_comment_time()) ?>
-           </a>
-           <?php edit_comment_link(__('(Edit)'),'  ','') ?>
-       </div>
-  
-       <?php comment_text() ?>
+       <span><?php comment_text() ?></span>
   
        <div class="reply">
           <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
